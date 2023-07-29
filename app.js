c421a817b33191e277e49cbba26f72aef8d61122
Stas Zaitsev <ststas@yandex.ru>
2023-07-29 16:44:29 +0300

    create routes and controllers  for USERS

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const usersRouter = require('./routes/users');
 
 const { PORT = 3000, ENDPOINT = 'mongodb://localhost:27017/mestodb' } = process.env;
 mongoose.connect(ENDPOINT);
 
 const app = express();
 
-app.listen(PORT, () => {console.log(`App listening on port ${PORT}`)});
+app.use('/users', usersRouter);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
