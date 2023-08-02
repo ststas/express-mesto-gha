0de3d695acae4021e4c08750d9af74123b1c9daf
Stas Zaitsev <ststas@yandex.ru>
2023-08-02 22:22:41 +0300

    fix error

--- errors/index.js
+++ errors/index.js
@@ -1,8 +1,8 @@
 function handleError(res, err) {
-  if (err.name === 'ValidationError') {
+  if (err.name === 'ValidationError' || err.name === 'CastError') {
     return res.status(400).send({ message: err.message });
   }
-  if (err.name === 'DocumentNotFoundError' || err.name === 'CastError') {
+  if (err.name === 'DocumentNotFoundError') {
     return res.status(404).send({ message: err.message });
   }
   return res.status(500).send({ message: 'На сервере произошла ошибка' });
