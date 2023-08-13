9b047dd754b2a37ddd196f8d9eb6b2addfc2aede
Stas Zaitsev <ststas@yandex.ru>
2023-08-13 21:08:55 +0300

    auth off

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
 const router = require('express').Router();
 const { handleRouteError } = require('../errors');
 const { validateSignUp, validateSignIn } = require('../middlewares/validation');
-const auth = require('../middlewares/auth');
+// const auth = require('../middlewares/auth');
 const { login, createUser } = require('../controllers/users');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 
 router.post('/signup', validateSignUp(), createUser);
 
-router.post('/signin', validateSignIn(), auth, login);
-router.use('/users', auth, usersRouter);
-router.use('/cards', auth, cardsRouter);
+router.post('/signin', validateSignIn(), login);
+router.use('/users', usersRouter);
+router.use('/cards', cardsRouter);
+
+// router.post('/signin', validateSignIn(), auth, login);
+// router.use('/users', auth, usersRouter);
+// router.use('/cards', auth, cardsRouter);
 
 router.all('*', handleRouteError);
 
