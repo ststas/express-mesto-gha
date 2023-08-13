48bb5baa6073273f6e5e8f05460625c15c568c3a
Stas Zaitsev <ststas@yandex.ru>
2023-08-14 00:46:57 +0300

    update auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,14 +8,14 @@ module.exports = (req, res, next) => {
   // const { authorization } = req.headers;
   // if (!authorization || !authorization.startsWith('Bearer ')) {
   if (!authorization) {
-    return handleWrongCredentials(res, 'Authorization is required');
+    return next(handleWrongCredentials(res, 'Authorization is required'));
   }
   let payload;
 
   try {
     payload = jwt.verify(authorization, NODE_ENV === 'production' ? JWT_SECRET : 'my-very-secret-key');
   } catch (err) {
-    return handleWrongCredentials(res, 'Authorization is required');
+    next(handleWrongCredentials(res, 'Authorization is required'));
   }
 
   req.user = payload;
