1e72d5a38f12d50b82baca76df13bc96a9e0d3ad
Stas Zaitsev <ststas@yandex.ru>
2023-08-13 20:30:40 +0300

    update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { handleWrongCredentials } = require('../errors');
 
-// const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -14,8 +14,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'my-very-secret-key');
-    // payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'my-very-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'my-very-secret-key');
   } catch (err) {
     return handleWrongCredentials(res, 'Authorization is required');
   }
